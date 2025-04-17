e4ccd25d2d45ca524c6298fc7efe5cfc563975f1
Gauthier MANON <gauthier.man.leo@gmail.com>
2025-04-17 14:40:17 +0400

    update script.js

--- script.js
+++ script.js
@@ -1,40 +1,89 @@
 // HTML Elements
-const cardContainer = document.querySelector(".card-container")
+const cardContainer = document.querySelector(".card-container");
+const searchInput = document.querySelector("#searchInput");
+const btnDateHigh = document.querySelector("#btnDateHigh");
+const btnDateLow = document.querySelector("#btnDateLow");
+// const iconSearch = document.querySelector(".fa-magnifying-glass")
+const rangeValue = document.querySelector("#rangeValue");
+const booksRange = document.querySelector("#booksRange")
 
+let sorType = "";
 
 // TODO : Récupérer / fetch une liste de livres 
-const apiUrl = "https://openlibrary.org/search.json?q=the+lord+of+the+rings";
+const apiUrl = "https://openlibrary.org/search.json?q=the+lord+of+the+rings?20";
 var books = [];
 async function getBooks() {
     const request = await fetch(apiUrl);
     const data = await request.json();
     books = data.docs;
     console.log("Données reçues", data);
-
+    
     showBooks();
 }
+
 
 getBooks();
 // TODO : Afficher la liste de livres
 function showBooks() {
-books.forEach(b=> {
     cardContainer.innerHTML= "";
+    let list = [...books];
+    //Filtrer 
+    const search = searchInput.value;
+    // const search2 = searchInput.value.toLowerCase();
+    if(search !=""){
+        
+        list = list.filter(b=> b.first_publish_year==search);
+        // list = list.filter(b=> b.title.toLowerCase().includes(search));
+    }
+
+    // Trier
+    if(sorType =="DateHigh") list.sort((a,b)=>b.first_publish_year - a.first_publish_year);
+    if(sorType =="DateLow") list.sort((a, b) => a.first_publish_year - b.first_publish_year);
+
+    //Range
+
+   
+list.forEach(b=> {
     const card = `
     <div class="card">
                <img
-                 src="${b.cover_i}"
+                 src="https://covers.openlibrary.org/b/id/${b.cover_i}-L.jpg"
                  alt="image du livre"
                />
                <h3>${b.title}</h3>
+               <h4>${b.author_name}</h4>
+               <p>${b.first_publish_year}</p>
               
              </div>
     
-    
-   
-   `;
+    `;
 
    cardContainer.innerHTML += card;
+
 })
+
+
+searchInput.addEventListener("input", showBooks);
+
+// iconSearch.addEventListener("click", () => {
+//     //Filtrer 
+//     const iconSearchInput = iconSearch.value;
+//     if(iconSearchInput  !=""){
+//         list = list.filter(b=> b.first_publish_year==search);
+//     }
+// })
+
+btnDateHigh.addEventListener("click", ()=> {
+    sorType = "DateHigh"
+    showBooks();
+
+})
+
+btnDateLow.addEventListener("click", ()=> {
+    sorType = "DateLow"
+    showBooks();
+})
+ 
 
 }
 
@@ -58,5 +107,7 @@ books.forEach(b=> {
 */
 
 // TODO : Filtrer par date, avec l'input searchInput
+
+
 
 // TODO : Trier par date, Date +, Date -
